6c9600b199073e71a8d1fa3b40df3c8a32a35a96
toasty <89341766+toastyyyxd@users.noreply.github.com>
2023-07-31 17:50:38 +0800

    Update antiafk.lua

--- antiafk.lua
+++ antiafk.lua
@@ -1,7 +1,16 @@
 -- from https://v3rmillion.net/showthread.php?tid=772135
 local vu = game:GetService("VirtualUser")
-game:GetService("Players").LocalPlayer.Idled:connect(function()
-   vu:Button2Down(Vector2.new(0,0),workspace.CurrentCamera.CFrame)
-   wait(1)
-   vu:Button2Up(Vector2.new(0,0),workspace.CurrentCamera.CFrame)
-end)
+local event
+
+local function enable() 
+   event = game:GetService("Players").LocalPlayer.Idled:connect(function()
+      vu:Button2Down(Vector2.new(0,0),workspace.CurrentCamera.CFrame)
+      wait(1)
+      vu:Button2Up(Vector2.new(0,0),workspace.CurrentCamera.CFrame)
+   end)
+end
+local function disable()
+   event:Disconnect()
+end
+
+return enable, disable
