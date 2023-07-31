b8541a5db208992a7b743c087f26d21627604645
toasty <89341766+toastyyyxd@users.noreply.github.com>
2023-07-31 17:40:39 +0800

    Update and rename privsrc/gold.lua to gold.lua

--- privsrc/gold.lua
+++
@@ -1,7 +0,0 @@
-local afkGold = loadstring(game:HttpGet'https://raw.githubusercontent.com/urmomjklol69/GoldFarmBabft/main/GoldFarm.lua')
-local function handler()
-    return Promise.new(function ()
-        afkGold()
-    end)
-end
-return handler
