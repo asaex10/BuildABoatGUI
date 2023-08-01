74d09204089c47461d5a733332e3f2fb679fcc4a
toasty <89341766+toastyyyxd@users.noreply.github.com>
2023-08-01 22:59:31 +0800

    Update rejoin.lua

--- rejoin.lua
+++ rejoin.lua
@@ -1,4 +1,15 @@
-local TPS = game:GetService("TeleportService")
-return function() 
+local TPS, HTTP = game:GetService("TeleportService"), game:GetService("HttpService")
+local function sameserver() 
   TPS:TeleportToPlaceInstance(game.PlaceId, game.JobId, game:GetService("Players").LocalPlayer)
+end
+local function diffserver()
+  local Servers = HttpService:JSONDecode(game:HttpGet("https://games.roblox.com/v1/games/"..game.PlaceId.."/servers/Public?sortOrder=Asc&limit=100"))
+  local possible = {}
+  for _, Server in Servers["data"] do
+    if Server["id"] ~= game.JobId then
+     table.insert(possible, Server)
+    end
+  end
+  local server = possible[Random.new():NextInteger(1, #possible)]
+  return TPService:TeleportToPlaceInstance(game.PlaceId, server)
 end
